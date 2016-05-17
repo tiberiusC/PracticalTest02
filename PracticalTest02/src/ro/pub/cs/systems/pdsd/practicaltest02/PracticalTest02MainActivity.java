da61f1811c46edd2fae5bc5a9d5ad9ecdfe2d6d5
Tiberius Ciuca <student@cti.pub.ro>
2016-05-17 09:40:17 +0300

    Task For PracticalTest02

---
+++ PracticalTest02/src/ro/pub/cs/systems/pdsd/practicaltest02/PracticalTest02MainActivity.java
@@ -0,0 +1,120 @@
+package ro.pub.cs.systems.pdsd.practicaltest02;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class PracticalTest02MainActivity extends Activity {
+
+	EditText portServer, porttClient, adressaToServer, word;
+	Button btnStart, btnConnect;
+	private ServerThread serverThread = null;
+    private ClientThread clientThread = null;
+    TextView tv;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test02_main);
+        portServer = (EditText)findViewById(R.id.server_port_edit_text);
+        porttClient = (EditText) findViewById(R.id.idPortClient);
+        adressaToServer = (EditText) findViewById(R.id.idAdresa);
+        word = (EditText) findViewById(R.id.idCuvant);
+        btnStart = (Button) findViewById(R.id.connect_button);
+        btnConnect = (Button) findViewById(R.id.idConnectClient);
+        tv = (TextView) findViewById(R.id.idText_view);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	 String serverPort = portServer.getText().toString();
+         	    if (serverPort == null || serverPort.isEmpty()) {
+         	      Toast.makeText(
+         	        getApplicationContext(),
+         	        "Server port should be filled!",
+         	        Toast.LENGTH_SHORT
+         	      ).show();
+         	      return;
+         	    }
+         	    serverThread = new ServerThread(Integer.parseInt(serverPort));
+         	    if (serverThread.getServerSocket() != null) {
+         	      serverThread.start();
+         	      Toast.makeText( getApplicationContext(),"Serverul a pornit!", Toast.LENGTH_SHORT).show();
+         	    } else {
+         	      Log.e(Constants.TAG, "[MAIN ACTIVITY] Could not creat server thread!");
+         	    }
+         	  
+            }
+        });
+        
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+          
+            @Override
+            public void onClick(View view) {
+                String clientAddress = adressaToServer.getText().toString();
+                String clientPort    = porttClient.getText().toString();
+                if (clientAddress == null || clientAddress.isEmpty() ||
+                        clientPort == null || clientPort.isEmpty()) {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Client connection parameters should be filled!",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    return;
+                }
+
+                if (serverThread == null || !serverThread.isAlive()) {
+                    Log.e(Constants.TAG, "[MAIN ACTIVITY] There is no server to connect to!");
+                    return;
+                }
+
+                String word1 = word.getText().toString();
+               
+                
+
+                tv.setText("");
+
+               clientThread = new ClientThread(
+                        clientAddress,
+                        Integer.parseInt(clientPort),
+                        word1,
+                       
+                      tv);
+                clientThread.start();
+            }
+            	
+        });
+    
+        
+    }
+ 
+    
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.practical_test02_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
